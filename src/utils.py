706fa048b87a7a8908708af9b68c7838929dc869
Maksim Charin <bonfiglioly@gmail.com>
2025-03-03 17:59:44 +0300

    add utils.py

---
+++ src/utils.py
@@ -0,0 +1,32 @@
+import json
+import os
+
+from src.product import Product
+from src.category import Category
+from config import DATA_DIR
+
+file_path = os.path.join(DATA_DIR, 'products.json')
+
+def read_json(path: str) -> list:
+    """Получение данных о товарах из json-файла"""
+    with open(path, 'r', encoding="UTF-8") as products_file:
+        products_data = json.load(products_file)
+        return products_data
+
+def create_objects_from_json(data):
+    categories = []
+    for category in data:
+        products = []
+        for item in category['products']:
+            products.append(Product(**item))
+        categories.append(Category(**category))
+
+    return categories
+
+
+
+if __name__ == '__main__':
+    raw_data = read_json(file_path)
+    print(create_objects_from_json(raw_data))
+    prod_data = create_objects_from_json(raw_data)
+    print(prod_data[0].description)
