869bce575ea505847274839e9e420d4a3bd80246
Maksim Charin <bonfiglioly@gmail.com>
2025-03-03 12:48:07 +0300

    add Product and Category

---
+++ src/category.py
@@ -0,0 +1,19 @@
+from src.product import Product
+
+
+class Category:
+    """Описание класса с категорией товара"""
+    name: str
+    description: str
+    products: list
+
+    category_count = 0
+    product_count = 0
+
+    def __init__(self, name, description, products):
+        self.name = name
+        self.description = description
+        self.products = products
+
+        Category.category_count += 1
+        Category.product_count += len(products)
