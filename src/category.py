8fcdfcd0217ee8df46b73ac861858ce38bf6fbfc
Maksim Charin <bonfiglioly@gmail.com>
2025-03-03 22:34:19 +0300

    add htmlcov dir and update Category class

--- src/category.py
+++ src/category.py
@@ -8,10 +8,10 @@ class Category:
     category_count = 0
     product_count = 0
 
-    def __init__(self, name, description, products=None):
+    def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products if products else []
+        self.products = products
 
         Category.category_count += 1
         Category.product_count += len(products) if products else 0
