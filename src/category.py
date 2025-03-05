614b29577a3e684acdb542ee440617a4dee07c88
Maksim Charin <bonfiglioly@gmail.com>
2025-03-06 00:05:26 +0300

    add category private att and getter

--- src/category.py
+++ src/category.py
@@ -1,3 +1,6 @@
+from src.product import Product
+
+
 class Category:
     """Описание класса с категорией товара"""
 
@@ -11,7 +14,21 @@ class Category:
     def __init__(self, name, description, products):
         self.name = name
         self.description = description
-        self.products = products
+        self.__products = products
 
         Category.category_count += 1
-        Category.product_count += len(products) if products else 0
+        Category.product_count += len(self.__products) if products else 0
+
+
+    def add_product(self, product: Product):
+        """Добавление единичного товара в список товаров"""
+        self.__products.append(product)
+
+        Category.product_count += 1
+
+    @property
+    def products(self):
+        product_str = ""
+        for product in self.__products:
+            product_str = f"{product.name}, {product.price} руб. Остаток: {product.quantity} шт."
+        return product_str
